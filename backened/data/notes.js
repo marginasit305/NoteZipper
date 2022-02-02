8bcf52dbdd2a106156d4f3822f672f29a6a91e71
marginasit305 <marginasit305@gmail.com>
2022-02-02 18:16:40 +0530

    Frontend and Backened

---
+++ backened/data/notes.js
@@ -0,0 +1,32 @@
+const notes = [
+  {
+    _id: "1",
+    title: "Day 1 of college",
+    content:
+      "I made a few new friends and introduced myself to a lot of new teachers.",
+    category: "College",
+  },
+  {
+    _id: "2",
+    title: "Learned some MERN Stack",
+    content:
+      "Learned how to create a server in node JS with frontend in reactjs and database in mongodb.",
+    category: "Learning",
+  },
+  {
+    _id: "3",
+    title: "Attended the fest at the college",
+    content:
+      "Participated in all the events held at department and college level in the fest.",
+    category: "Events",
+  },
+  {
+    _id: 4,
+    title: "Wedding of cousin",
+    content:
+      "Enjoyed a lot in the wedding with all the cousins, did dances with all",
+    category: "Entertainment",
+  },
+];
+
+module.exports = notes;
